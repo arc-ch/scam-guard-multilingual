22754f285ccd03039c896dd5a618296a011082b1
Archit Choudhury <134518231+arc-ch@users.noreply.github.com>
2025-06-11 13:54:47 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ export default {
   plugins: [],
 };
 
-module.exports = {
-  // ... your existing config
-  safelist: [
-    'bg-red-500/20',
-    'text-red-500',
-    'bg-red-500',
-    'text-red-600',
-    // Add any other conditional classes you use
-  ],
-  // ... rest of config
-}
+// module.exports = {
+//   // ... your existing config
+//   safelist: [
+//     'bg-red-500/20',
+//     'text-red-500',
+//     'bg-red-500',
+//     'text-red-600',
+//     // Add any other conditional classes you use
+//   ],
+//   // ... rest of config
+// }
